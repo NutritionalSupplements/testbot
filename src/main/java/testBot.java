8d300fe32079508886877a3023801ecfb62c8009
NDudenchenko <n.dudenchenko@gmail.com>
2019-09-23 17:15:01 +0300

    Revert "Test commit"
    This reverts commit 2867aa38

--- src/main/java/testBot.java
+++ src/main/java/testBot.java
@@ -25,7 +25,6 @@ public class testBot extends TelegramLongPollingBot {
 
     private TesseractProcessing tesseractProcessing = new TesseractProcessing();
 
-//Daroy
 
     public void onUpdateReceived(Update update) {
         SendMessage message = new SendMessage();
